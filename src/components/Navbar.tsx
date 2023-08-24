0757880d45c366d0c8424d947c4286b9044bcf81
storialp <96848889+storialp@users.noreply.github.com>
2023-08-25 01:17:57 +0200

    For syncing with pc

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,88 @@
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline/index.js";
+
+interface NavbarProps {
+  path: string;
+}
+
+const navigation = [
+  { name: "Home", href: "/" },
+  { name: "Divisions", href: "/divisions" },
+  { name: "Alumni", href: "/alumni" },
+  { name: "Join us", href: "/join" },
+];
+
+export default function Navbar({ path }: NavbarProps) {
+  return (
+    <Disclosure as="nav" className="bg-[#3C3C3B] shadow">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 justify-between">
+              <div className="flex">
+                <div className="flex flex-shrink-0 items-center ">
+                  <img
+                    className="block h-12 w-36 lg:hidden rounded-full"
+                    src="https://personal-website-pics-2.s3.eu-central-1.amazonaws.com/BSVC+-+Logo+Orizzontale+3.png"
+                    alt="A picture of me"
+                  />
+                  <img
+                    className="hidden h-12 w-36 lg:block rounded-full"
+                    src="https://personal-website-pics-2.s3.eu-central-1.amazonaws.com/BSVC+-+Logo+Orizzontale+3.png"
+                    alt="A picture of me"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                  {navigation.map((item) => (
+                    <a
+                      href={item.href}
+                      key={item.name}
+                      className={
+                        path === item.href
+                          ? "inline-flex items-center border-b-2 border-[#FFDE00] px-1 pt-1 text-sm font-medium text-[#FFFFFF]"
+                          : "inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-100 hover:border-gray-300 hover:text-gray-700"
+                      }
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+              <div className="-mr-2 flex items-center sm:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 pt-2 pb-3">
+              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  as="a"
+                  href={item.href}
+                  className={
+                    path === item.href
+                      ? "block border-l-4 border-yellow-400 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-yellow-700"
+                      : "block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                  }
+                  key={item.name}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
