582404863b19ea323dc864d20ebd599230399e25
storialp <luis.pericchi@studbocconi.it>
2024-09-09 12:38:35 +0200

    Updated application form

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -11,7 +11,7 @@ export default function CallToAction() {
         <a
           type="button"
           className="inline-flex mt-4 items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-orange-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-          href="https://forms.gle/GwgHYpeKq1bMQHaJ8"
+          href="https://forms.gle/nJEbG3XLBEfErNoVA"
         >
           Apply now!
         </a>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,4 @@
 export default function Hero() {
-
   return (
     <div className="bg-gray-900">
       <div className="relative isolate overflow-hidden">
@@ -16,15 +15,18 @@ export default function Hero() {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
-              Applications are open{' '}
-              <a href="https://forms.gle/GwgHYpeKq1bMQHaJ8" className="font-semibold text-white">
+              Applications are open{" "}
+              <a
+                href="https://forms.gle/nJEbG3XLBEfErNoVA"
+                className="font-semibold text-white"
+              >
                 <span className="absolute inset-0 pl-1" aria-hidden="true" />
                 Apply now <span aria-hidden="true">&rarr;</span>
               </a>
@@ -40,12 +42,15 @@ export default function Hero() {
             </p> */}
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="https://forms.gle/GwgHYpeKq1bMQHaJ8"
+                href="https://forms.gle/nJEbG3XLBEfErNoVA"
                 className="rounded-md bg-yellow-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-400"
               >
                 Join now
               </a>
-              <a href="#divisions" className="text-sm font-semibold leading-6 text-white">
+              <a
+                href="#divisions"
+                className="text-sm font-semibold leading-6 text-white"
+              >
                 About us <span aria-hidden="true">→</span>
               </a>
             </div>
@@ -59,11 +64,11 @@ export default function Hero() {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
